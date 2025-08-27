ee1a25b29798c5514497105d10dac8309fd0188e
Duyet <duyetvan03@gmail.com>
2025-08-27 15:02:41 +0700

    init project

---
+++ apps/gateway/src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { GatewayModule } from './gateway.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(GatewayModule);
+
+  await app.listen(3000); // Gateway chính HTTP API
+  console.log(`Gateway is running on http://localhost:3000`);
+}
+bootstrap();
